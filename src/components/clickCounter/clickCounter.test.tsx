f5d5fa48ac03ad7ba35f677556bdd31eb9fea7a1
shomrkm <shochan1985@gmail.com>
2021-08-26 22:35:58 +0900

    feat: Implement clickCounter component.

--- src/components/clickCounter/clickCounter.test.tsx
+++ src/components/clickCounter/clickCounter.test.tsx
@@ -1,25 +1,53 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, ShallowWrapper } from 'enzyme';
 import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 import { ClickCounter } from './clickCounter';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+/**
+ * Factory function to create a ShallowWrapper for the ClickCounter component.
+ * @function setup
+ * @return {ShallowWrapper}
+ */
+const setup = ()=> shallow(<ClickCounter />);
+
+const findByTestAttr = (wrapper: ShallowWrapper, val: string) => wrapper.find(`[data-test='${val}']`);
+
 test('render without error', ()=>{
-  const wrapper = shallow(<ClickCounter />);
-  const clickCounter = wrapper.find("[data-test='component-click-counter']");
+  const wrapper = setup();
+  const clickCounter = findByTestAttr(wrapper, "component-click-counter");
   expect(clickCounter.length).toBe(1);
 })
 
 test('renders increment button', ()=>{
-  
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, "increment-button");
+  expect(button.length).toBe(1);
 })
+
 test('renders counter display', ()=>{
-  
+  const wrapper = setup();
+  const counterDisplay= findByTestAttr(wrapper, "counter-display");
+  expect(counterDisplay.length).toBe(1);
 })
+
 test('counter display starts at 0', ()=>{
-  
+  const wrapper = setup();
+  const count = findByTestAttr(wrapper, "count").text();
+  expect(count).toBe("0");
 })
+
 test('clicking button increments counter display', ()=>{
-  
+  const wrapper = setup();
+
+  // find the button 
+  const button = findByTestAttr(wrapper, "increment-button");
+
+  // click the button
+  button.simulate('click');
+
+  // find the display, and test that the number has been incremented
+  const count = findByTestAttr(wrapper, "count").text();
+  expect(count).toBe("1");
 })
--- src/components/clickCounter/clickCounter.tsx
+++ src/components/clickCounter/clickCounter.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 interface Props {
 
 }
 
 export const ClickCounter: React.VFC<Props> = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <div data-test="component-click-counter">
-      <h1>App</h1>
+      <h1 data-test="counter-display">
+        The counter is currently &ensp;
+        <span data-test="count">{count}</span>
+        </h1>
+      <button 
+        data-test="increment-button"
+        onClick={()=>setCount(count+1)}>
+          Increment counter
+      </button>
     </div>
   );
 }
