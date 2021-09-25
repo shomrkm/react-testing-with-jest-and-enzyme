bf434c13dbefb764295058e65de6078b1a52b078
shomrkm <shochan1985@gmail.com>
2021-09-25 16:02:08 +0900

    feat: Implement Jotto.

--- src/components/jotto/Jotto.test.tsx
+++ src/components/jotto/Jotto.test.tsx
@@ -1,13 +1,45 @@
-import { shallow } from "enzyme";
+/**
+ * @jest-environment jsdom
+ */
+
+import { mount } from "enzyme";
 import { findByTestAttr } from '../../../test/testUtils'
 import { Jotto } from './Jotto'
 
+// activate global mock to make sure getSecretWord doesn't make network call
+jest.mock('./actions');
+import { getSecretWord as mockGetSecretWord } from './actions';
+
 const setup = ()=>{
-  return shallow(<Jotto />);
+  // use mount , because useEffect not called on `shallow`
+  // https://github.com/airbnb/enzyme/issues/2086
+  return mount(<Jotto />);
 }
 
 test('renders without error', ()=>{
   const wrapper = setup();
   const jotto = findByTestAttr(wrapper, 'jotto-app');
   expect(jotto.length).toBe(1);
+})
+
+describe('get secret word', ()=>{
+  beforeEach(()=>{
+    // clear the mock calls from previous tests
+    (mockGetSecretWord as jest.Mock).mockClear();
+  })
+  test('get secret word', ()=>{
+    const wrapper = setup();
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(1);
+  })
+  test('get secretWord does not run on app update', ()=>{
+    const wrapper = setup();
+    (mockGetSecretWord as jest.Mock).mockClear();
+
+    // using setProps because wrapper.update() doesn't trigger useEffect
+    // https://github.com/enzymejs/enzyme/issues/2254
+    wrapper.setProps({});
+
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(0);
+  })
+
 })
--- src/components/jotto/Jotto.tsx
+++ src/components/jotto/Jotto.tsx
@@ -1,27 +1,35 @@
-
-import React from 'react'
-import { Congrats } from './Congrats'
-import { GuessedWords } from './GuessedWords'
+import React, { useEffect } from 'react'
 import styled from 'styled-components';
+
+import { Congrats } from './Congrats'
+import { GuessedWordList, GuessedWords } from './GuessedWords'
 import { Input } from './Input';
+import { getSecretWord } from './actions';
 
 const Container = styled.div`
   text-align: center;
 `
 
-interface Props {}
+interface Props {
+}
 
 export const Jotto: React.VFC<Props> = () => {
+  // TODO: get props from shared state
+  const success = false;
+  const secretWord = 'party';
+  const guessedWords:GuessedWordList = [];
+
+  useEffect(() => {
+    getSecretWord();
+  }, []);
+
   return (
     <Container>
       <div data-test="jotto-app" className="jotto-app">
         <h1>Jotto</h1>
-        <Congrats success={false} />
-        <Input success={false} secretWord='abcde'/>
-        <GuessedWords guessedWords={[
-          {guessedWord:'train', letterMatchCount: 3},
-          {guessedWord:'apple', letterMatchCount: 2}
-        ]} />
+        <Congrats success={success} />
+        <Input success={success} secretWord={secretWord} />
+        <GuessedWords guessedWords={guessedWords} />
       </div>
     </Container>
   );
